f2fc2a8fb3a3c5a3ba97e2d981e9e8960964c31e
Dorovane <dorovaneelemperador@gmail.com>
2025-08-22 03:14:30 +0100

    h

--- config/db.js
+++ config/db.js
@@ -1,28 +1,22 @@
 const { Sequelize } = require("sequelize");
-const DB_NAME=process.env.DB_NAME
-const DB_USER=process.env.DB_USER
-const DB_PASSWORD=process.env.DB_PASSWORD
-const HOST=process.env.HOST
-const DIALECT=process.env.DIALECT
 
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = process.env.HOST; 
+const DIALECT = process.env.DIALECT;
 
-const sequelize=new Sequelize(
-  DB_NAME,DB_USER,DB_PASSWORD,{
-    host:HOST,
-    dialect:DIALECT
-  }
-)
-
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: HOST,
+  dialect: DIALECT,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false } 
+  },
+  logging: false
+});
 
 sequelize.authenticate()
-.then(()=>{
-  console.log('Connexion reussie')
-})
-.catch((error)=>{
-  console.error(error)
-})
+  .then(() => console.log("Connexion réussie à Supabase !"))
+  .catch((error) => console.error(error));
 
-
-module.exports=sequelize
-
-
+module.exports = sequelize;
